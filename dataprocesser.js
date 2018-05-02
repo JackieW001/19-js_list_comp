3790bd45661c52b77687f97031448e94ad998b0e
JackieW001 <jwoo@stuy.edu>
2018-05-01 21:07:14 -0400

    added filtering based on key and value

--- dataprocesser.js
+++ dataprocesser.js
@@ -1,19 +1,56 @@
+
+
 // Track population for each age
 var neutralPop = data.map(function(a) {
     return a.total;
 });
 
-console.log("Population per age group");
-console.log(neutralPop);
 
 // Total population
 
 var totalPops = neutralPop.reduce(function(x, y){return x + y})
 
-console.log("Total population: " + totalPops);
+var tot_pop = document.createElement("h3")
+tot_pop.appendChild(document.createTextNode("Total Population"));
+document.body.appendChild(tot_pop);
+
+var tot_pop_data = document.createElement("p")
+tot_pop_data.appendChild(document.createTextNode(totalPops));
+document.body.appendChild(tot_pop_data);
+//console.log("Total population: " + totalPops);
+
+
 
 // Average population across all ages
 
 var average = totalPops / neutralPop.length;
 
-console.log("Average population per age group: " + average);
+var avg_pop = document.createElement("h3")
+avg_pop.appendChild(document.createTextNode("Average Population"));
+document.body.appendChild(avg_pop);
+
+var avg_pop_data = document.createElement("p")
+avg_pop_data.appendChild(document.createTextNode(average));
+document.body.appendChild(avg_pop_data);
+//console.log("Average population per age group: " + average);
+
+
+
+// Filtering data based on key and value
+
+var filter_data = function(ret_key, key, value){
+    filtered = data.filter(function(x) {return x[key] > value;})
+    return filtered.map(function(x) {return x[ret_key];})
+};
+
+var filtered = document.createElement("h3")
+filtered.appendChild(document.createTextNode("Ages that have more than 2000000 Females"));
+document.body.appendChild(filtered);
+
+var filtered_data = document.createElement("p")
+filtered_data.appendChild(document.createTextNode(filter_data("age", "females", 2000000)))
+document.body.appendChild(filtered_data);
+
+
+// Return ages with female count over 2000000
+console.log(filter_data("age","females", 2000000))
